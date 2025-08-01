46dd4ec055bae675a57723902b2edf92332fd300
Harper Rhett <harperrhett@gmail.com>
2025-08-01 15:15:08 -0500

    Tweaked values

--- Program.cs
+++ Program.cs
@@ -8,4 +8,4 @@ const int windowSize = 800;
 
 new QuadtreeTest(windowSize, 500);
 new SpatialHash2DTest(windowSize, 100, 100, 500);
-new SpatialHash3DTest(windowSize, 100, 25, 50, 100);
+new SpatialHash3DTest(windowSize, 100, 25, 100, 200);
